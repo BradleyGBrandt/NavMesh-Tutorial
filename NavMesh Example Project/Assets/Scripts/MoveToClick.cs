1796ff361eb24ffcab5e61eb1fcbf3c6fcfa3b15
GBrandt <gbrandt@mail.bradley.edu>
2020-12-06 13:50:02 -0800

    Initial commit

---
+++ NavMesh Example Project/Assets/Scripts/MoveToClick.cs
@@ -0,0 +1,35 @@
+﻿/*
+* (Greg Brandt)
+* (Assignment 9)
+* Moves the player to where the mouse is clicked
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClick : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+		{
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+			{
+                agent.destination = hit.point;
+			}
+		}
+    }
+}
